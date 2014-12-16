1798917564e29f5ef2f476713f9fcdbc819b005d
crazyjohn <magicstoneljg@163.com>
2014-12-16 22:00:27 +0800

    server
    server

---
+++ game/src/com/stone/game/GameServer.java
@@ -0,0 +1,16 @@
+package com.stone.game;
+
+/**
+ * 游戏服务器;
+ * 
+ * @author Administrator
+ *
+ */
+public class GameServer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
