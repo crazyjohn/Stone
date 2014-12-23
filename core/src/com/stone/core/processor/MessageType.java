50466d041efbb3649c751f935c136cbffe32c420
crazyjohn <magicstoneljg@163.com>
2014-12-23 18:59:06 +0800

    mina
    mina

--- core/src/com/stone/core/processor/MessageType.java
+++ core/src/com/stone/core/processor/MessageType.java
@@ -8,6 +8,20 @@ package com.stone.core.processor;
  */
 public enum MessageType {
 	/** 客户端请求装备技能 */
-	CG_EQUIP_SKILL
+	CG_EQUIP_SKILL(1001),;
+	
+	private int type;
+	
+	MessageType(int type) {
+		this.setType(type);
+	}
+
+	public int getType() {
+		return type;
+	}
+
+	public void setType(int type) {
+		this.type = type;
+	}
 
 }
