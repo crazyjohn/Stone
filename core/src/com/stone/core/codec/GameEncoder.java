25cd9db98d129d1057d4304a5f246833dcc0d74c
crazyjohn <magicstoneljg@163.com>
2014-12-24 22:55:39 +0800

    encoder
    encoder

--- core/src/com/stone/core/codec/GameEncoder.java
+++ core/src/com/stone/core/codec/GameEncoder.java
@@ -4,6 +4,9 @@ import org.apache.mina.core.session.IoSession;
 import org.apache.mina.filter.codec.ProtocolEncoder;
 import org.apache.mina.filter.codec.ProtocolEncoderOutput;
 
+import com.stone.core.msg.IMessage;
+import com.stone.core.msg.IProtobufMessage;
+
 /**
  * 游戏编码器;
  * 
@@ -16,13 +19,16 @@ public class GameEncoder implements ProtocolEncoder {
 	public void encode(IoSession session, Object message,
 			ProtocolEncoderOutput out) throws Exception {
 		// TODO Auto-generated method stub
-
+		IMessage msg = (IMessage) message;
+		if (msg instanceof IProtobufMessage) {
+			
+		}
+		//out.write(encodedMessage);
 	}
 
 	@Override
 	public void dispose(IoSession session) throws Exception {
 		// TODO Auto-generated method stub
-
 	}
 
 }
