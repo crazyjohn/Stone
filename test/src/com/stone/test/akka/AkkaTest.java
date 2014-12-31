4d53beee63f4ee85410e494af0545ebe6081f472
crazyjohn <magicstoneljg@163.com>
2014-12-31 19:17:56 +0800

    akka
    add akka

---
+++ test/src/com/stone/test/akka/AkkaTest.java
@@ -0,0 +1,14 @@
+package com.stone.test.akka;
+
+import akka.actor.Actor;
+
+public class AkkaTest {
+
+	public static void main(String[] args) {
+		Actor actor = null;
+		if (actor == null) {
+			// do something
+		}
+	}
+
+}
