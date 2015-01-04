e6c135051a446795d21c50e5a70af3f09b9a70cd
crazyjohn <magicstoneljg@163.com>
2015-01-04 14:38:38 +0800

    add bot
    add bot project

---
+++ bot/src/com/stone/bot/handler/Handlers.scala
@@ -0,0 +1,27 @@
+package com.stone.bot.handler
+
+import com.stone.core.msg.IMessage
+import com.stone.bot.CrazyBot
+import java.util.HashMap
+import com.stone.core.msg.IProtobufMessage
+/**
+ * Handler static factory;
+ */
+object Handlers {
+  private val handlers = new HashMap[Integer, (IMessage, CrazyBot) => Unit]()
+
+  
+
+  def handle(msg: IProtobufMessage, bot: CrazyBot) = {
+    handlers.get(msg.getType) match {
+      case null => 
+        println("No handler for type: " + msg.getType)
+      case handler => 
+        handler(msg, bot) 
+    }
+  }
+  
+  def registHandler(msgType: Int, handler:(IMessage, CrazyBot)=>Unit) {
+    handlers.put(msgType, handler)
+  }
+}
