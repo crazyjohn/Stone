f1b436238f04aa08332b4283586dc51a0def3d66
crazyjohn <magicstoneljg@163.com>
2015-01-28 22:09:11 +0800

    comment
    comment

--- actor/src/com/stone/actor/future/IActorFuture.java
+++ actor/src/com/stone/actor/future/IActorFuture.java
@@ -2,17 +2,52 @@ package com.stone.actor.future;
 
 import com.stone.actor.listener.IActorFutureListener;
 
+/**
+ * Actor's future pattern;
+ * 
+ * @author crazyjohn
+ * 
+ * @param <T>
+ */
 public interface IActorFuture<T> {
 
+	/**
+	 * 返回执行结果;
+	 * 
+	 * @return
+	 */
 	public T getResult();
 
+	/**
+	 * 设置结果;
+	 * 
+	 * @param result
+	 */
 	public void setResult(T result);
 
+	/**
+	 * 是否已经设置好了结果;
+	 * 
+	 * @return
+	 */
 	public boolean isReady();
 
+	/**
+	 * 触发准备好;
+	 */
 	public void ready();
 
+	/**
+	 * 添加关心future结果的监听器;
+	 * 
+	 * @param listener
+	 */
 	public void addListener(IActorFutureListener<T> listener);
-	
+
+	/**
+	 * 移除监听器;
+	 * 
+	 * @param listener
+	 */
 	public void removeListener(IActorFutureListener<T> listener);
 }
