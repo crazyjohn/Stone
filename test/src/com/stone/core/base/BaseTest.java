b1cbdf7db81f35a06417729781fa9cf0930f1f70
crazyjohn <magicstoneljg@163.com>
2015-01-28 11:53:04 +0800

    junit
    add unit test;

--- test/src/com/stone/core/base/BaseTest.java
+++ test/src/com/stone/core/base/BaseTest.java
@@ -1,9 +1,16 @@
 package com.stone.core.base;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class BaseTest {
 
-	public static void main(String[] args) {
-		System.out.println(new Integer(1) == new Integer(1));
+	@Test
+	public void integerTest() {
+		// false
+		Assert.assertFalse(new Integer(1) == new Integer(1));
+		// true
+		Assert.assertTrue(new Integer(1).equals(new Integer(1)));
 	}
 
 }
