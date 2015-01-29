f75cb4c3b9ea0dd5f827f1ed55c93affcefcd93a
crazyjohn <magicstoneljg@163.com>
2015-01-29 17:55:58 +0800

    future
    future

--- actor/src/com/stone/actor/future/ActorFuture.java
+++ actor/src/com/stone/actor/future/ActorFuture.java
@@ -14,6 +14,7 @@ import com.stone.core.annotation.ThreadSafeUnit;
 public class ActorFuture<T> implements IActorFuture<T> {
 	protected volatile boolean isReady = false;
 	/** the execution result */
+	@GuardedByUnit(whoCareMe = "ReadWriteLock")
 	protected T result;
 	/** read write lock */
 	protected ReadWriteLock resultLock;
@@ -73,6 +74,8 @@ public class ActorFuture<T> implements IActorFuture<T> {
 	@Override
 	public void ready() {
 		this.isReady = true;
+		// notify all
+		this.notifyAll();
 	}
 
 	@Override
@@ -83,6 +86,22 @@ public class ActorFuture<T> implements IActorFuture<T> {
 	@Override
 	public synchronized void removeListener(IActorFutureListener<T> listener) {
 		listeners.remove(listener);
+	}
+
+	@Override
+	public synchronized T awaitResult() throws InterruptedException {
+		while (!isReady) {
+			this.wait();
+		}
+		return result;
+	}
+
+	@Override
+	public T awaitResult(long timeout) throws InterruptedException {
+		while (!isReady) {
+			this.wait(timeout);
+		}
+		return result;
 	}
 
 }
--- actor/src/com/stone/actor/future/IActorFuture.java
+++ actor/src/com/stone/actor/future/IActorFuture.java
@@ -12,11 +12,30 @@ import com.stone.actor.listener.IActorFutureListener;
 public interface IActorFuture<T> {
 
 	/**
-	 * 返回执行结果;
+	 * 返回执行结果(Balking pattern);<br>
+	 * 不会阻塞，如果结果没有执行完成，则会立即返回；
 	 * 
-	 * @return
+	 * @return 可能会返回null，此种情况发生在结果没有执行完成的情况下;
 	 */
 	public T getResult();
+
+	/**
+	 * 阻塞等待结果(Guarder suspension pattern);
+	 * 
+	 * @return
+	 * @throws InterruptedException
+	 *             可中断, 中断抛出此异常;
+	 */
+	public T awaitResult() throws InterruptedException;
+
+	/**
+	 * 带超时的阻塞等待结果;
+	 * 
+	 * @param timeout
+	 * @return
+	 * @throws InterruptedException
+	 */
+	public T awaitResult(long timeout) throws InterruptedException;
 
 	/**
 	 * 设置结果;
