e90f404bc4dd3a417ebf4029db32c3c78f805f6b
crazyjohn <magicstoneljg@163.com>
2015-02-02 19:41:51 +0800

    refact
    add actor to core

--- core/src/com/stone/core/msg/handler/BaseProtobufMessageHandler.java
+++
@@ -1,7 +0,0 @@
-package com.stone.core.msg.handler;
-
-import com.stone.core.msg.IProtobufMessage;
-
-public abstract class BaseProtobufMessageHandler implements
-		IMessageHandlerWithType<IProtobufMessage> {
-}
