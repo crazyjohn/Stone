e7ec12c7e8820d9cdf0123ef232bac97cf948f36
crazyjohn <magicstoneljg@163.com>
2015-03-06 21:36:39 +0800

    routee
    routees

--- test/src/com/stone/core/akka/router/Master.java
+++ test/src/com/stone/core/akka/router/Master.java
@@ -5,25 +5,34 @@ import java.util.List;
 
 import akka.actor.ActorRef;
 import akka.actor.Props;
+import akka.actor.Terminated;
 import akka.actor.UntypedActor;
-import akka.routing.RoutedActorRef;
+import akka.routing.ActorRefRoutee;
+import akka.routing.RoundRobinRoutingLogic;
 import akka.routing.Router;
 
 public class Master extends UntypedActor {
 	Router router;
 	{
-		List<RoutedActorRef> routees = new ArrayList<RoutedActorRef>();
+		List<ActorRefRoutee> routees = new ArrayList<ActorRefRoutee>();
 		for (int i = 0; i < 5; i++) {
-			ActorRef r = this.getContext().actorOf(Props.create(Work.class));
-			this.getContext().watch(r);
-			//routees.add(new RoutedActorRef(r));
+			ActorRef actor = this.getContext().actorOf(Props.create(Work.class));
+			this.getContext().watch(actor);
+			routees.add(new ActorRefRoutee(actor));
 		}
-		//router = new Router(new Roundrobin);
+		router = new Router(new RoundRobinRoutingLogic());
 	}
+
 	@Override
 	public void onReceive(Object msg) throws Exception {
-		// TODO Auto-generated method stub
-		
+		if (msg instanceof Work) {
+			router.route(msg, getSender());
+		} else if (msg instanceof Terminated) {
+			router = router.removeRoutee(((Terminated)msg).actor());
+			ActorRef actor = getContext().actorOf(Props.create(Work.class));
+			getContext().watch(actor);
+			router = router.addRoutee(new ActorRefRoutee(actor));
+		}
 	}
 
 }
