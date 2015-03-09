d3dd46fe7cae179f243c34cce1a6db59b3310e95
crazyjohn <magicstoneljg@163.com>
2015-03-09 22:29:42 +0800

    bot
    bot

--- bot/src/com/stone/bot/BotApp.scala
+++ bot/src/com/stone/bot/BotApp.scala
@@ -8,12 +8,18 @@ import org.slf4j.LoggerFactory
  */
 object BotApp extends App {
   private val logger = LoggerFactory.getLogger("ClientIoHandler")
-  val bot = new CrazyBot("bot1", "bot1")
-  val connectFuture = bot.connect("0.0.0.0", 8081)
-  connectFuture.awaitUninterruptibly();
-  bot.start()
-  bot.setSession(connectFuture.getSession)
-  connectFuture.getSession().setAttribute("bot", bot)
-  logger.info("Session opend: " + connectFuture.getSession)
-  bot.doLogin()
+  val begin = 1
+  val end = 5
+  // create robot
+  for (i <- begin to end) {
+    val bot = new CrazyBot("bot" + i, "bot" + i)
+    val connectFuture = bot.connect("0.0.0.0", 8081)
+    connectFuture.awaitUninterruptibly();
+    bot.start()
+    bot.setSession(connectFuture.getSession)
+    connectFuture.getSession().setAttribute("bot", bot)
+    logger.info("Session opend: " + connectFuture.getSession)
+    bot.doLogin()
+  }
+
 }
