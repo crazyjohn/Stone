4b48dbd0a1dd66a2d5fecc50da7e20d542c82f0f
crazyjohn <magicstoneljg@163.com>
2015-03-18 11:13:03 +0800

    subEntity
    handle sub entity db operation

--- db/src/com/stone/db/login/DBHumanActor.java
+++ db/src/com/stone/db/login/DBHumanActor.java
@@ -12,11 +12,14 @@ import akka.actor.UntypedActor;
 import com.stone.core.converter.IConverter;
 import com.stone.core.db.service.IDBService;
 import com.stone.core.entity.IEntity;
+import com.stone.core.entity.IHumanSubEntity;
 import com.stone.core.util.LRUHashMap;
 import com.stone.db.cache.HumanCache;
 import com.stone.db.entity.HumanEntity;
 import com.stone.db.entity.converter.HumanConverter;
+import com.stone.db.msg.internal.DBDeleteMessage;
 import com.stone.db.msg.internal.DBGetMessage;
+import com.stone.db.msg.internal.DBUpdateMessage;
 import com.stone.db.msg.internal.player.InternalSelectRoleResult;
 
 /**
@@ -27,6 +30,7 @@ import com.stone.db.msg.internal.player.InternalSelectRoleResult;
  */
 public class DBHumanActor extends UntypedActor {
 	private static final String TICK = "tick";
+	private static final int BATCH_UPDATE_COUNT = 100;
 	/** human cache */
 	private final LRUHashMap<Long, HumanCache> cache;
 	/** dbService */
@@ -56,8 +60,80 @@ public class DBHumanActor extends UntypedActor {
 			DBGetMessage getMsg = (DBGetMessage) msg;
 			onHumanQueryRequest(getMsg);
 		} else if (msg.equals(TICK)) {
-			// tick
-			logger.info("Time to update the dirty human cache.");
+			// handle tick request
+			logger.error("Time to update the dirty human cache.");
+			handleTickRequest();
+		} else if (msg instanceof DBUpdateMessage) {
+			DBUpdateMessage updateMsg = (DBUpdateMessage) msg;
+			// handle update request
+			handleUpdateRequest(updateMsg);
+
+		} else if (msg instanceof DBDeleteMessage) {
+			DBDeleteMessage deleteMsg = (DBDeleteMessage) msg;
+			// handle delete request
+			handleDeleteRequest(deleteMsg);
+		}
+	}
+
+	/**
+	 * Handle delete request;
+	 * 
+	 * @param deleteMsg
+	 */
+	private void handleDeleteRequest(DBDeleteMessage deleteMsg) {
+		if (deleteMsg.getEntity() instanceof IHumanSubEntity) {
+			// handle sub human entity
+			IHumanSubEntity humanSubEntity = (IHumanSubEntity) deleteMsg.getEntity();
+			HumanCache humanCache = this.cache.get(humanSubEntity.getHumanGuid());
+			if (humanCache == null) {
+				logger.warn(String.format("Human cache is null, the humaGuid is: %d", humanSubEntity.getHumanGuid()));
+			}
+			// remove sub entity
+			humanCache.remove(humanSubEntity.getClass(), humanSubEntity.getId());
+		} else {
+			logger.warn(String.format("MFucker are you kidding me? DBHumanActor do not handle this class: %s", deleteMsg.getEntityClass()
+					.getSimpleName()));
+		}
+	}
+
+	/**
+	 * Handle tick request;
+	 */
+	private void handleTickRequest() {
+		// not a good way
+		int updateCount = 0;
+		for (HumanCache eachCache : this.cache.values()) {
+			// break when up to max update count
+			if (updateCount >= BATCH_UPDATE_COUNT) {
+				break;
+			}
+			if (eachCache.isModified()) {
+				HumanEntity eachEntity = this.converter.convertTo(eachCache);
+				this.dbService.update(eachEntity);
+				eachCache.resetModified();
+				updateCount++;
+			}
+		}
+	}
+
+	/**
+	 * Handle update request;
+	 * 
+	 * @param updateMsg
+	 */
+	private void handleUpdateRequest(DBUpdateMessage updateMsg) {
+		if (updateMsg.getEntity() instanceof IHumanSubEntity) {
+			// handle sub human entity
+			IHumanSubEntity humanSubEntity = (IHumanSubEntity) updateMsg.getEntity();
+			HumanCache humanCache = this.cache.get(humanSubEntity.getHumanGuid());
+			if (humanCache == null) {
+				logger.warn(String.format("Human cache is null, the humaGuid is: %d", humanSubEntity.getHumanGuid()));
+			}
+			// update sub entity
+			humanCache.update(humanSubEntity);
+		} else {
+			logger.warn(String.format("MFucker are you kidding me? DBHumanActor do not handle this class: %s", updateMsg.getEntityClass()
+					.getSimpleName()));
 		}
 	}
 
