dd360153abf78cf0e3a76bfb45751451ee704265
crazyjohn <magicstoneljg@163.com>
2015-04-14 22:51:30 +0800

    test
    test

--- test/src/com/stone/core/jvm/HeapTest.java
+++ test/src/com/stone/core/jvm/HeapTest.java
@@ -3,11 +3,8 @@ package com.stone.core.jvm;
 public class HeapTest {
 
 	public static void main(String[] args) {
-		for (int i = 0; i < 100; i++) {
-			System.out.println(i);
-			byte[] data = new byte[1024 * 1024 * 1024];
-			System.out.println(data.length);
-		}
+		byte[] data = new byte[1024 * 1024 * 6];
+		System.out.println(data.length);
 	}
 
 }
