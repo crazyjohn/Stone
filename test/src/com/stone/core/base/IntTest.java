fbed85b5e3384fafd7f6b0b40e87cccc936e0111
crazyjohn <magicstoneljg@163.com>
2015-05-23 10:37:42 +0800

    Int test
    int test

---
+++ test/src/com/stone/core/base/IntTest.java
@@ -0,0 +1,12 @@
+package com.stone.core.base;
+
+public class IntTest {
+
+	public static void main(String[] args) {
+		long harm1 = Long.MAX_VALUE;
+		long harm2 = 1;
+		int between = (int) (harm1 - harm2);
+		System.out.println(between);
+	}
+
+}
