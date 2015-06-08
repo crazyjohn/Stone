e00b578e93f5fbaecdb5cc6711d9fdedd3ab8cea
crazyjohn <magicstoneljg@163.com>
2015-06-08 15:53:39 +0800

    regular
    regular

--- test/src/com/stone/core/regular/RegularTest.java
+++ test/src/com/stone/core/regular/RegularTest.java
@@ -1,24 +1,54 @@
 package com.stone.core.regular;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.regex.Pattern;
 
+/**
+ * Regular test;
+ * 
+ * @author crazyjohn
+ *
+ */
 public class RegularTest {
 	/** header */
 	public static final Pattern TEMPLATE_OBJECT_HEADER = Pattern
 			.compile("template\\s+([^\\s]+)\\s*\\{");
 	/** whole body */
 	public static final Pattern TEMPLATE_OBJECT = Pattern
-			.compile("template\\s+([^\\s]+)\\s*\\{\\s*(([^\\s]+)\\s+([^\\s]+)\\s*;\\s*(//\\s*?(.*))?)+\\}");
+			.compile("\\s*template\\s+([^\\s]+)\\s*\\{\\s*(([^\\s]+)\\s+([^\\s]+)\\s*;\\s*(//\\s*(.*)\\s*+)?)+\\}\\s*");
+	/** templates */
+	public static final Pattern TEMPLATE_OBJECTS = Pattern
+			.compile("\\s*template\\s+([^\\s]+)\\s*\\{\\s*(([^\\s]+)\\s+([^\\s]+)\\s*;\\s*(//\\s*(.*)\\s*+)?)+\\}\\s*");
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+		// head
 		System.out.println("Match 'template Item {' : "
 				+ TEMPLATE_OBJECT_HEADER.matcher("template Item {").matches());
-		System.out
-				.println("Match 'template ItemLevelUp {int level; // level int costCoin;// costCoin String desc; // desc}' : "
-						+ TEMPLATE_OBJECT
-								.matcher(
-										"template ItemLevelUp {int level; // level int costCoin;// costCoin String desc; // desc}")
-								.matches());
+		// body
+		String targetString = "template ItemLevelUp { \n"
+				+ "int level; // level \n" + "int costCoin;// costCoin \n"
+				+ "String desc;\n" + "} ";
+		System.out.println("Match --'\n" + targetString + "\n' : "
+				+ TEMPLATE_OBJECT.matcher(targetString).matches());
+		// read from file
+		FileReader in = new FileReader(new File(System.getProperty("user.dir")
+				+ "/resources/template/Item.templ"));
+		BufferedReader reader = new BufferedReader(in);
+		StringBuilder content = new StringBuilder();
+		String line = null;
+		while ((line = reader.readLine()) != null) {
+			content.append(line + "\n");
+		}
+		// print content
+		System.out.println(content.toString());
+		// match
+		System.out.println(TEMPLATE_OBJECTS.matcher(content.toString())
+				.matches());
+		reader.close();
+
 	}
 
 }
