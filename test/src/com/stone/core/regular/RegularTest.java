03249fb4552932a04d9c99d809d95a0d02e8818d
crazyjohn <magicstoneljg@163.com>
2015-06-08 13:56:09 +0800

    regular
    regular

--- test/src/com/stone/core/regular/RegularTest.java
+++ test/src/com/stone/core/regular/RegularTest.java
@@ -3,23 +3,21 @@ package com.stone.core.regular;
 import java.util.regex.Pattern;
 
 public class RegularTest {
+	/** header */
 	public static final Pattern TEMPLATE_OBJECT_HEADER = Pattern
 			.compile("template\\s+([^\\s]+)\\s*\\{");
+	/** whole body */
 	public static final Pattern TEMPLATE_OBJECT = Pattern
-			.compile("template\\s+([^\\s]+)\\s*\\{\\s*\\}");
+			.compile("template\\s+([^\\s]+)\\s*\\{\\s*(([^\\s]+)\\s+([^\\s]+)\\s*;\\s*(//\\s*?(.*))?)+\\}");
 
 	public static void main(String[] args) {
 		System.out.println("Match 'template Item {' : "
 				+ TEMPLATE_OBJECT_HEADER.matcher("template Item {").matches());
-		System.out.println("Match 'template Item{' : "
-				+ TEMPLATE_OBJECT_HEADER.matcher("template Item{").matches());
-		System.out.println("Match 'templateItem {' : "
-				+ TEMPLATE_OBJECT_HEADER.matcher("templateItem {").matches());
 		System.out
-				.println("Match 'template ItemLevelUp {int level;// 等级 int costCoin;//消耗货币 String desc; //desc}' : "
+				.println("Match 'template ItemLevelUp {int level; // level int costCoin;// costCoin String desc; // desc}' : "
 						+ TEMPLATE_OBJECT
 								.matcher(
-										"template ItemLevelUp {int level;// 等级 int costCoin;//消耗货币 String desc; //desc}")
+										"template ItemLevelUp {int level; // level int costCoin;// costCoin String desc; // desc}")
 								.matches());
 	}
 
