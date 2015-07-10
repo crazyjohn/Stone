e40481d7e8ae29a113516f27ea910785c88bc0d4
crazyjohn <magicstoneljg@163.com>
2015-07-10 15:31:57 +0800

    delete usseless

--- game/src/com/stone/game/msg/handler/BaseProtobufMessageHandler.java
+++
@@ -1,8 +0,0 @@
-package com.stone.game.msg.handler;
-
-import com.stone.game.handler.IMessageHandlerWithType;
-import com.stone.game.msg.ProtobufMessage;
-
-public abstract class BaseProtobufMessageHandler implements
-		IMessageHandlerWithType<ProtobufMessage> {
-}
--- game/src/com/stone/game/msg/internal/InternalPlayerAuthSucceed.java
+++
@@ -1,5 +0,0 @@
-package com.stone.game.msg.internal;
-
-public class InternalPlayerAuthSucceed {
-
-}
