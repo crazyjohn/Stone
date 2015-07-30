690b4c124917baa04bb5584b0a57565989fdd27b
crazyjohn <magicstoneljg@163.com>
2015-07-30 20:21:57 +0800

    uuid

--- core/src/com/stone/core/data/uuid/IUUID64.java
+++ core/src/com/stone/core/data/uuid/IUUID64.java
@@ -8,11 +8,33 @@ package com.stone.core.data.uuid;
  */
 public interface IUUID64 {
 
+	/**
+	 * Get next id;
+	 * 
+	 * @return
+	 */
 	public long getNextId();
 
+	/**
+	 * Get current id;
+	 * 
+	 * @return
+	 */
 	public long getCurrentId();
 
+	/**
+	 * Get the regionId;
+	 * 
+	 * @param uuid
+	 * @return
+	 */
 	public int getRegionId(long uuid);
 
+	/**
+	 * Get the serverId;
+	 * 
+	 * @param uuid
+	 * @return
+	 */
 	public int getServerId(long uuid);
 }
--- core/src/com/stone/core/data/uuid/UUID64.java
+++ core/src/com/stone/core/data/uuid/UUID64.java
@@ -2,13 +2,22 @@ package com.stone.core.data.uuid;
 
 import java.util.concurrent.atomic.AtomicLong;
 
+/**
+ * The UUID64 struct;
+ * <P>
+ * Total 63 bits = regionId (5 bits) + serverId (10 bits) + objectId (45 bits) +
+ * other (3 bits)
+ * 
+ * @author crazyjohn
+ *
+ */
 public class UUID64 implements IUUID64 {
+	// constants
 	private static final int TOTAL_BITS = 63;
 	private static final int REGION_BITS = 5;
 	private static final int SERVER_BITS = 10;
 	private static final int OBJECT_BITS = 45;
-	public static final int OTHER_BITS = TOTAL_BITS - REGION_BITS - SERVER_BITS
-			- OBJECT_BITS;
+	public static final int OTHER_BITS = TOTAL_BITS - REGION_BITS - SERVER_BITS - OBJECT_BITS;
 	private static final int INIT_VALUE = 1;
 	// bits
 	private int regionBits;
@@ -25,8 +34,7 @@ public class UUID64 implements IUUID64 {
 	private AtomicLong idGenerator;
 	private long maxValue;
 
-	public UUID64(int regionBits, int serverBits, int objectBits, int regionId,
-			int serverId, int initValue) {
+	public UUID64(int regionBits, int serverBits, int objectBits, int regionId, int serverId, int initValue) {
 		this.regionBits = regionBits;
 		this.serverBits = serverBits;
 		this.objectBits = objectBits;
@@ -36,26 +44,29 @@ public class UUID64 implements IUUID64 {
 		checkValue(regionId, regionBits);
 		checkValue(serverId, serverBits);
 		// high and mask
-		long high = getRealValue(regionId, (TOTAL_BITS - regionBits));
+		long high = computeLeftMoveValue(regionId, (TOTAL_BITS - regionBits));
 		this.regionIdMask = getMaskValue(regionBits, TOTAL_BITS - regionBits);
-		high = high
-				| (getRealValue(serverId,
-						(TOTAL_BITS - regionBits - serverBits)));
-		this.serverIdMask = getMaskValue(serverBits, TOTAL_BITS - regionBits
-				- serverBits);
+		high = high | (computeLeftMoveValue(serverId, (TOTAL_BITS - regionBits - serverBits)));
+		this.serverIdMask = getMaskValue(serverBits, TOTAL_BITS - regionBits - serverBits);
 		// max value
 		maxValue = Long.MAX_VALUE;
 		// id
 		this.idGenerator = new AtomicLong(high | initValue);
 	}
 
-	private long getRealValue(long value, int bits) {
+	private long computeLeftMoveValue(long value, int bits) {
 		return value << bits;
 	}
 
+	/**
+	 * Build the default UUID;
+	 * 
+	 * @param regionId
+	 * @param serverId
+	 * @return
+	 */
 	public static IUUID64 buildDefaultUUID(int regionId, int serverId) {
-		IUUID64 uuid = new UUID64(REGION_BITS, SERVER_BITS, OBJECT_BITS,
-				regionId, serverId, INIT_VALUE);
+		IUUID64 uuid = new UUID64(REGION_BITS, SERVER_BITS, OBJECT_BITS, regionId, serverId, INIT_VALUE);
 		return uuid;
 	}
 
@@ -64,15 +75,28 @@ public class UUID64 implements IUUID64 {
 		return maxValue << shiftBits;
 	}
 
+	/**
+	 * Check if the value up to the max;
+	 * 
+	 * @param value
+	 * @param bits
+	 * 
+	 * @throws IllegalArgumentException
+	 *             when the value up to the max;
+	 */
 	private void checkValue(int value, int bits) {
 		long maxValue = getMaxValue(bits);
 		if (value > maxValue) {
-			throw new IllegalArgumentException(String.format(
-					"The max value is: %d, you fucker's value is: %d",
-					maxValue, value));
+			throw new IllegalArgumentException(String.format("The max value is: %d, you fucker's value is: %d", maxValue, value));
 		}
 	}
 
+	/**
+	 * Get the max value;
+	 * 
+	 * @param bits
+	 * @return
+	 */
 	private long getMaxValue(int bits) {
 		return (1 << bits) - 1;
 	}
@@ -82,8 +106,7 @@ public class UUID64 implements IUUID64 {
 		long nextId = this.idGenerator.incrementAndGet();
 		if (nextId > maxValue) {
 			this.idGenerator.set(maxValue);
-			throw new IllegalStateException(String.format(
-					"Id up to the max value: %d", nextId));
+			throw new IllegalStateException(String.format("Id up to the max value: %d", nextId));
 		}
 		return idGenerator.get();
 	}
@@ -100,8 +123,7 @@ public class UUID64 implements IUUID64 {
 
 	@Override
 	public int getServerId(long uuid) {
-		return (int) ((this.serverIdMask & uuid) >> (TOTAL_BITS
-				- this.regionBits - this.serverBits));
+		return (int) ((this.serverIdMask & uuid) >> (TOTAL_BITS - this.regionBits - this.serverBits));
 	}
 
 	public int getRegionId() {
