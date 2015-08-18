82cd32139cdbaf2ac354bd2a1d966bc14646247c
crazyjohn <magicstoneljg@163.com>
2015-08-18 16:54:31 +0800

    run

--- bot/src/com/stone/bot/BotApp.scala
+++ bot/src/com/stone/bot/BotApp.scala
@@ -13,7 +13,7 @@ object BotApp extends App {
   // create robot
   for (i <- begin to end) {
     val bot = new CrazyBot("bot" + i)
-    val connectFuture = bot.connect("0.0.0.0", 8081)
+    val connectFuture = bot.connect("203.195.218.172", 8081)
     connectFuture.awaitUninterruptibly()
     bot.start()
     bot.setSession(connectFuture.getSession)
