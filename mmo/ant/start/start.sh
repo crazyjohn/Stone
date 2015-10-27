40b3b9fafa0d8815fe0eb6a6faf671faa03983e4
crazyjohn <magicstoneljg@163.com>
2015-10-27 18:47:34 +0800

    start script

--- mmo/ant/start/start.sh
+++ mmo/ant/start/start.sh
@@ -1,3 +1,1 @@
->./console.log
-nohup java -cp ".:lib/*" -Xms4096m -Xmx4096m -Xmn1024M -Xss2M -XX:MaxTenuringThreshold=20 -XX:+HeapDumpOnOutOfMemoryError com/stone/launcher/Launcher >>./console.log &
-tail -f console.log
+nohup java -cp "mmo.jar:lib/*" -Xms4096m -Xmx4096m -Xmn1024M -Xss2M -XX:MaxTenuringThreshold=20 -XX:+HeapDumpOnOutOfMemoryError com/stone/launcher/Launcher > console.log & tail -f console.log
