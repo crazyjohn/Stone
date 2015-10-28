645320a31afd29a92b4d1bc978081e20c2c3da60
crazyjohn <magicstoneljg@163.com>
2015-10-28 18:41:00 +0800

    mmo

--- builder/mmo/start/start.bat
+++ builder/mmo/start/start.bat
@@ -1,1 +1,1 @@
-java -cp ".;lib/*" -Xms4096m -Xmx4096m -Xmn1024M -Xss2M -XX:MaxTenuringThreshold=20 -XX:+HeapDumpOnOutOfMemoryError com/stone/launcher/Launcher
+java -cp "mmo.jar;lib/*" -Xms4096m -Xmx4096m -Xmn1024M -Xss2M -XX:MaxTenuringThreshold=20 -XX:+HeapDumpOnOutOfMemoryError com/stone/launcher/Launcher
